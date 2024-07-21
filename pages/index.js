8d6b593cd3437ee7ce05c012ad5055cfc35bc550
fkolenez <129692741+fkolenez@users.noreply.github.com>
2024-07-21 17:52:28 +0000

    adiciona scripts ´lint:check´ e ´lint:fix´

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-function Home(){
-   return <h1>Oi amorzinho!!!</h1>
+function Home() {
+  return <h1>Oi amorzinho!!!</h1>;
+}
+
+function teste() {
+  console.log("teste 1");
+}
+
+function teste2() {
+  console.log("teste 1");
 }
 
 export default Home;
