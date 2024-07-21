eea4c73e2e1703d117e754fba171882358a6ec46
fkolenez <129692741+fkolenez@users.noreply.github.com>
2024-07-21 18:04:56 +0000

    adiciona o arquivo .prettierignore

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Oi amorzinho!!!</h1>;
 }
 
-function teste() {
-  console.log("teste 1");
-}
-
-function teste2() {
-  console.log("teste 1");
-}
-
 export default Home;
